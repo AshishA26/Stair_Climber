97e86cb9f64e79ea7c4af972940ccee2a7003c33
AlphaLead18 <samuelke0604@gmail.com>
2023-11-03 09:02:37 -0400

    Added Sensor Configuration
    Sensor Numbers still need to be Determined/Added

--- Final_Project.c
+++ Final_Project.c
@@ -4,7 +4,29 @@
   11/03/2023
 */
 
+void configureSensors();
+
 task main()
 {
+	configureSensors();
 
+}
+
+void configureSensors() //Sensor #s to be determined and added
+{
+	SensorType[] = sensorEV3_Touch;
+
+	SensorType[] = sensorEV3_Ultrasonic;
+
+	SensorType[] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorMode[] = modeEV3Color_Color;
+	wait1Msec(50);
+
+	SensorType[] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorMode[] = modeEV3Gyro_Calibration;
+	wait1Msec(100);
+	SensorMode[] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
 }
