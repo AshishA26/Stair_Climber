ccaac687a521c7980c0c86940ddb2acabd76e237
Ashish Agrahari <shinyflame007@gmail.com>
2023-11-23 22:09:22 -0500

    Changed sound threshold

--- Final_Project.c
+++ Final_Project.c
@@ -53,7 +53,7 @@ void moveBeltDown(int motorPower, int amount);
 
 // Constants
 const int SPEED_SLOW = 20;
-const float SOUND_LEVEL = 20;
+const float SOUND_LEVEL = 50;
 const float TIME_INTERVAL = 10;
 const int CM_TO_ENC = 360 / (2.0 * PI * 3.4);
 const int ROBOT_LENGTH = 10;
